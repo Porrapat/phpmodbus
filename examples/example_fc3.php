66121961c63c9b659313bc57ad3261e04a1fa5ea
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2016-11-03 22:23:12 +0700

    Test 4

--- examples/example_fc3.php
+++ examples/example_fc3.php
@@ -16,24 +16,6 @@ catch (Exception $e) {
     exit;
 }
 
-// Print status information
-//echo "</br>Status:</br>" . $modbus;
-
-// Print read data
-//echo "</br>Data:</br>";
-//print_r($recData); 
-//echo "</br>";
-
-//echo json_encode($recData);
-// Get signed integer from INT interpretation
-//echo "<h3>INT to integer </h3>\n";
-//foreach($values as $bytes)
-//    echo PhpType::bytes2signedInt($bytes) . "</br>";
-// Chunk the data array to set of 4 bytes
-
-
-//foreach($values as $bytes)
-//    echo PhpType::bytes2signedInt($bytes) . "</br>";
 $values = array_chunk($recData, 2);
 
 $tojson = array(
@@ -41,7 +23,6 @@ $tojson = array(
     'irr2' => PhpType::bytes2signedInt($values[1]),
     'irr3' => PhpType::bytes2signedInt($values[2])
 );
-
 
 header('Content-type: text/html; charset=UTF-8');
 echo json_encode($tojson);
